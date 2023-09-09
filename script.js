c37bec256dc21736e00864b348161b0fbd6385c1
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-09 21:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let button = document.getElementById("incrementBtn");
+let numbers = document.getElementById("counter");
+var count = 0;
+
+counter.innerHTML = count;
+button.addEventListener("click",myfunction);
+function myfunction(){
+	alert(count);
+	count++;
+	counter.innerHTML = count;
+}
